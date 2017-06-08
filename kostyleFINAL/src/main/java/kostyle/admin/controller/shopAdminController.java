0af5ac865dc116511b9212f1347284388f38e209
kosta-PC\kosta <sjinjooo@gmail.com>
2017-06-08 18:03:18 +0900

    20170608 JJ modal에서 shop->adshop 등록하기

--- kostyleFINAL/src/main/java/kostyle/admin/controller/shopAdminController.java
+++ kostyleFINAL/src/main/java/kostyle/admin/controller/shopAdminController.java
@@ -1,7 +1,9 @@
 package kostyle.admin.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -11,13 +13,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import kostyle.admin.domain.AdShoppingMallAdmin;
+import kostyle.admin.domain.CriteriaAdmin;
+import kostyle.admin.domain.PageMakerAdmin;
+import kostyle.admin.domain.SearchCriteriaAdmin;
 import kostyle.admin.domain.ShopStateAdmin;
 import kostyle.admin.domain.ShoppingMallAdmin;
 import kostyle.admin.service.ShopAdminService;
@@ -43,19 +52,6 @@ public class shopAdminController {
 		logger.info("insert post");
 		logger.info(shop.toString());
 		
-		int countShop = service.countShoppingMall()+1;
-		String s_Num;
-		
-		if(countShop < 10){
-			System.out.println("s_0"+countShop);
-			s_Num = "s_0"+countShop;
-		}else{
-			System.out.println("s_"+countShop);
-			s_Num = "s_"+countShop;
-		}
-		
-		shop.setS_num(s_Num);
-		
 		service.insertShoppingMall(shop);
 		rttr.addFlashAttribute("msg", "SUCCESS");
 		return "redirect:/admin/shopList";
@@ -64,10 +60,9 @@ public class shopAdminController {
 	
 	//shoppingmall list
 	@RequestMapping(value="/shopList", method=RequestMethod.GET)
-	
-	public void shopListGET(ShoppingMallAdmin shop, Model model)throws Exception{
+	public void shopListGET(@ModelAttribute("cri") SearchCriteriaAdmin cri, Model model)throws Exception{
 		
-		List<ShoppingMallAdmin> shopList = service.shopList();
+		List<ShoppingMallAdmin> shopList = service.shopList(cri);
 		List<AdShoppingMallAdmin> adShopList = service.adShopList();
 		List<ShopStateAdmin> stateList = new ArrayList<ShopStateAdmin>();
 		
@@ -88,6 +83,14 @@ public class shopAdminController {
 		}
 		
 		model.addAttribute("list", stateList);
+		
+		//페이징
+		PageMakerAdmin pm = new PageMakerAdmin();
+		
+		pm.setCri(cri);
+		pm.setTotalCount(service.countSearchShop(cri));
+		
+		model.addAttribute("page", pm);
 	}
 	
 	//shoppingmall 리스트에서 adshoppingmall로 등록하기
@@ -97,7 +100,7 @@ public class shopAdminController {
 		logger.info("adShoppingMall post/s_num: "+s_num);
 
 		for(int i=0;i<s_num.length;i++){
-			System.out.println(s_num[i]);
+			System.out.println(s_num[i]+" 등록합니다");
 			ShoppingMallAdmin shop = service.getShoppingMall(s_num[i]);
 			service.insertAdShoppingMall(shop);
 		}
@@ -108,11 +111,33 @@ public class shopAdminController {
 	
 	//모달창에서 adshoppingmall 등록하기
 	@RequestMapping(value="/shopModal", method=RequestMethod.POST)
-	public ResponseEntity<String> shopModal(@RequestBody ShoppingMallAdmin shopModal){
+	public ResponseEntity<String> shopModal(@RequestBody Map<String, Object> paramMap){
 		ResponseEntity<String> entity = null;
 		try {
-			ShoppingMallAdmin shop = service.getShoppingMall(shopModal.getS_num());
-			service.insertAdShoppingMall(shop);
+			
+			Map<String, Object> shopMap = new HashMap<String, Object>();
+			
+			ObjectMapper mapper = new ObjectMapper();
+			
+			String shopJson = (String) paramMap.get("shop");
+			
+			shopMap = mapper.readValue(shopJson, new TypeReference<Map<String, String>>(){});
+			
+			System.out.println("controller-----------------------------------");
+			System.out.println(shopMap);
+			System.out.println(shopMap.get("s_num"));
+			System.out.println(shopMap.get("s_sname"));
+			System.out.println(shopMap.get("s_shopurl"));
+			System.out.println(shopMap.get("s_searchurl"));
+			System.out.println(shopMap.get("s_shopreg"));
+			System.out.println(shopMap.get("s_age"));
+			System.out.println(shopMap.get("s_manager"));
+			System.out.println(shopMap.get("s_phonenumber"));
+			System.out.println(shopMap.get("s_email"));
+			System.out.println(shopMap.get("s_image"));
+			System.out.println(paramMap.get("s_grade"));
+//			ShoppingMallAdmin shop = service.getShoppingMall(shopModal.getS_num());
+//			service.insertAdShoppingMall(shop);
 			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -121,13 +146,12 @@ public class shopAdminController {
 		return entity;
 	}
 	
-	//!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//리스트에서 adshoppingmall 등록 취소하기
 	@RequestMapping(value="/deleteAdShop", method=RequestMethod.POST)
 	public String deleteAdShop(String[] s_num, RedirectAttributes rttr)throws Exception{
-		System.out.println("ddd");
 		
 		for(int i=0;i<s_num.length;i++){
-			System.out.println(s_num[i]);
+			System.out.println(s_num[i]+" 등록 취소합니다");
 			service.deleteAdShop(s_num[i]);
 		}
 		
@@ -135,23 +159,21 @@ public class shopAdminController {
 		return "redirect:/admin/shopList";
 	}
 	
-	
-	
-	//adshoppingmall 리스트
-	@RequestMapping(value="/adShopList", method=RequestMethod.GET)
-	public void adShopList(AdShoppingMallAdmin adshop, Model model)throws Exception{
-		model.addAttribute("list", service.adShopList());
+	//모달창에서 adshoppingmall 등록취소하기
+	@RequestMapping(value="/deleteAdShopModal", method=RequestMethod.POST)
+	public ResponseEntity<String> deleteAdShopModal(@RequestBody AdShoppingMallAdmin adShopModal){
+		ResponseEntity<String> entity = null;
+		try {
+			System.out.println(adShopModal.getS_num()+" 등록 취소합니다");
+			service.deleteAdShop(adShopModal.getS_num());
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
 	}
-	
-//	@RequestMapping(value="/shopDetail", method=RequestMethod.GET)
-//	public void shopDetail(@RequestParam("s_num") String s_num, Model model) throws Exception{
-//		model.addAttribute("ShoppingMallAdmin",service.shopDetail(s_num));
-//	}
-			
-//	@RequestMapping(value="/adShop", method=RequestMethod.GET)
-//	public String adShop(@RequestParam("s_num") String s_num)throws Exception{
-//		return "adShopList";
-//	}
+
 	
 //	@RequestMapping(value="/test", method=RequestMethod.GET)
 //	public void test(Model model)throws Exception{
@@ -159,12 +181,29 @@ public class shopAdminController {
 //	}
 //	
 //	@RequestMapping(value="/test", method=RequestMethod.POST)
-//	public String testPOST(ShoppingMallAdmin shop, RedirectAttributes rttr)throws Exception{
+//	public Map<String, Object> testPOST(@RequestBody Map<String, Object> params)throws Exception{
 //		
-//		System.out.println(shop.getS_num());
-//		rttr.addFlashAttribute("msg", "SUCCESS");
-//		
-//		return "redirect:/admin/shopList";
+//		 Map<String, Object> resultMap = new HashMap<String,Object>();
+//		 try{
+//		 Map<String, Object> shopMap = new HashMap<String,Object>();
+//		 Map<String, Object> adShopMap = new HashMap<String,Object>();
+//		 
+//		 ObjectMapper mapper = new ObjectMapper();
+//		 String json = (String) params.get("shop");
+//		 String adjson = (String) params.get("adshop");
+//		 
+//		 shopMap = mapper.readValue(json, new TypeReference<Map<String, String>>(){});
+//		 adShopMap = mapper.readValue(adjson, new TypeReference<Map<String, String>>(){});
+//		 		System.out.println(shopMap);
+//		 		System.out.println(shopMap.get("name"));
+//	            System.out.println(adShopMap);
+//	            System.out.println(adShopMap.get("water"));
+//	        }catch(Exception e){
+//	            resultMap.put("message",e.getMessage());
+//	            return resultMap;
+//	        }        
+//	        resultMap.put("message", "회원 정보가 정상적으로 변경되었습니다");
+//	        return resultMap;
 //	}
 	
 }
