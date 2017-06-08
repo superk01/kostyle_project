9442db67b42d6b5c63627f517a9f7d3775cfd951
SY <SY@LAPTOP-SY>
2017-06-08 12:27:27 +0900

    ..

--- kostyleFINAL/src/main/java/kostyle/search/service/SearchServiceImpl.java
+++ kostyleFINAL/src/main/java/kostyle/search/service/SearchServiceImpl.java
@@ -28,6 +28,7 @@ public class SearchServiceImpl implements SearchService{
 		List<String> url = searchDao.getSearchUrl();
 		List<JsoupThread> threads = new ArrayList<>();
 		for(int i=0; i<url.size(); i++){
+			System.out.println("url"+i+"번지 주소 확인:"+url.get(i));
 			threads.add(new JsoupThread(url.get(i)+keyword));
 			threads.get(i).start();
 		}
@@ -36,7 +37,7 @@ public class SearchServiceImpl implements SearchService{
 		for(int i=0; i<url.size(); i++){
 			int count=0;
 			while(count<url.size()){
-				if(threads.get(i).getState()==State.TERMINATED){ //TERMINATED = 쓰레드의 작업이 종료된 상태
+				if(threads.get(i).getState()==State.TERMINATED){
 					resultList=threads.get(i).getResult();
 					for(int j=0; j<resultList.size(); j++){
 						result.add(resultList.get(j));
