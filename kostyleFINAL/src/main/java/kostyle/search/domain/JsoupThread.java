fbf82db690c0ad8d04fc94e516c657cb16534744
kosta-PC\kosta <imbulsajo@gmail.com>
2017-06-08 19:39:44 +0900

    .

--- kostyleFINAL/src/main/java/kostyle/search/domain/JsoupThread.java
+++ kostyleFINAL/src/main/java/kostyle/search/domain/JsoupThread.java
@@ -14,6 +14,7 @@ public class JsoupThread extends Thread{
 	
 	private String path;
 	private List<SearchVO> result;
+	private List<String> nextPages;
 	
 	
 	
@@ -24,12 +25,21 @@ public class JsoupThread extends Thread{
 	public void setResult(List<SearchVO> result) {
 		this.result = result;
 	}
+	
+	public List<String> getNextPages() {
+		return nextPages;
+	}
+
+	public void setNextPages(List<String> nextPages) {
+		this.nextPages = nextPages;
+	}
 
 	public JsoupThread() {}
 	
 	public JsoupThread(String path){
 		this.path = path;
 		result = new ArrayList<SearchVO>();
+		nextPages = new ArrayList<String>();
 	}
 	
 	@Override
@@ -57,6 +67,7 @@ public class JsoupThread extends Thread{
 			for(int i=0; i<thumbnailEl.size(); i++){
 				thumbnail.add(thumbnailEl.get(i).attr("src"));
 			}
+			System.out.println("썸네일 이미지 검색수:"+thumbnail.size());
 			/*System.out.println("thumbnail:"+thumbnail);*/
 			/*System.out.println("thumbnail:"+thumbnail.size());*/
 			/*System.out.println(doc);*/
@@ -67,6 +78,7 @@ public class JsoupThread extends Thread{
 			for(int i=0; i<prdNameEl.size(); i++){
 				prdName.add(prdNameEl.get(i).html());
 			}
+			System.out.println("제품명 검색수:"+prdName.size());
 			/*System.out.println("prdName:"+prdName);*/
 			/*System.out.println("prdName:"+prdName.size());*/
 			
@@ -91,6 +103,11 @@ public class JsoupThread extends Thread{
 			}
 			prdUrl = new ArrayList<String>(new LinkedHashSet<String>(list));
 			
+			System.out.println("검색된 상품 url숫자:"+prdUrl.size());
+			/*System.out.println("검색된 상품 url내용:"+prdUrl);*/
+			for(int i=0; i<prdUrl.size(); i++){
+				System.out.println(i+"번째 링크:"+prdUrl.get(i));
+			}
 			/*for(int i=0; i<prdUrl.size(); i++){
 				System.out.println(prdUrl.get(i));
 			}*/
@@ -138,41 +155,61 @@ public class JsoupThread extends Thread{
 					prdPrice.add(list.get(i).substring(0,arr[i]));
 				}*/
 			}else{
-				/*Element el = doc.select("li.xans-record->span").first();
-				System.out.println(el);
-						doc.select("li.xans-record-");*/
 				prdPriceEl = doc.select("li.xans-record-:eq(0)>span");
-				/*prdPriceEl = prdPriceEl.eq(0);*/
-				/*System.out.println(prdPriceEl);*/
 				for(int i=0; i<prdPriceEl.size(); i++){
-					if(!(prdPriceEl.get(i).html().equals(""))){
+					/*if(!(prdPriceEl.get(i).html().equals(""))){*/
 						prdPrice.add(prdPriceEl.get(i).html());
-					}
+						System.out.println(i+"번째 가격정보: "+prdPriceEl.get(i).html());
+						
+					/*}*/
 				}
 			}
 			
+			System.out.println("검색된 가격 숫자:"+prdPrice.size());
+			/*System.out.println("검색된 가격 정보:"+prdPrice);*/
+			for(int i=0; i<prdPrice.size(); i++){
+				System.out.println(i+"번째 가격:"+prdPrice.get(i));
+			}
 			/*System.out.println(prdPrice);*/		
 			/*System.out.println("prdPrice:"+prdPrice.size());	*/	
 			
 			
 			for(int i=0; i<prdName.size(); i++){
 				result.add(new SearchVO(thumbnail.get(i), prdUrl.get(i), prdPrice.get(i), prdName.get(i)));
-				System.out.println(thumbnail.get(i)+","+prdUrl.get(i)+","+prdPrice.get(i)+","+prdName.get(i));
+				/*System.out.println(thumbnail.get(i)+","+prdUrl.get(i)+","+prdPrice.get(i)+","+prdName.get(i));*/
 			}
-			System.out.println(result);
+			/*System.out.println(result);*/
 			
 			//상품 검색을 두 페이지 이상 검색했을때...
 			//일단 예시로 스타일 난다.
-			List<String> list31 = new ArrayList<>();
+			List<String> list31 = new ArrayList<String>();
 			Elements nextPageEl = doc.select("div.xans-search-paging a.other");
 			System.out.println("선택자 잘 잡았습니까?"+nextPageEl);
 			for(int i=0; i<nextPageEl.size(); i++){
 				list31.add(nextPageEl.get(i).attr("href"));
 			}
+			String str1= null;
+			String str2= null;
 			if(list31 != null){
+				/*System.out.println("페이징 잘 뽑힙니까?"+list31);*/
+				if(path.indexOf(".com/")>0){
+					index = path.indexOf(".com/");
+				}else{
+					index = path.indexOf("o.kr/");
+				}
 				
+				str1 = path.substring(0, index+4);				//쇼핑몰 url: 예)http://www.stylenanda.com
+				System.out.println(str1);
+				int index2 = path.indexOf(".html?");
+				System.out.println(index2);
+				str2 = path.substring(index2-15, index2+5);
+				System.out.println("/product/search.html? 이거 맞음???"+str2);
 			}
-			System.out.println("페이징 잘 뽑힙니까?"+list31);
+			for(int i=0; i<list31.size(); i++){
+				System.out.println("최종 다음 검색 주소:"+str1+str2+list31.get(i));
+				nextPages.add(str1+str2+list31.get(i));
+			}
+			/*System.out.println("최종 다음 검색 주소:"+nextPages);*/
 			Thread.sleep(1000);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -180,8 +217,23 @@ public class JsoupThread extends Thread{
 		
 	}
 	
-/*public static void main(String[] args){
-		String path = "http://www.stylenanda.com/product/search.html?banner_action=&keyword=%EB%B0%94%EC%A7%80&page=2";
+	//테스트 하는 곳!!!
+public static void main(String[] args){
+		/*String path = "http://hotping.co.kr/product/search.html?banner_action=&order_by=favor&keyword=바지";*/
+		String path = "http://www.stylenanda.com/product/search.html?banner_action=&keyword=바지";
+		JsoupThread thread = new JsoupThread(path);
+		thread.start();
+	}
+	
+	/*public static void main(String[] args) {
+		http://66girls.co.kr/product/search.html?banner_action=&keyword=바지"; 통과
+			  http://www.stylenanda.com/product/search.html?banner_action=&keyword=바지 통과
+				http://ggsing.com/product/search.html?banner_action=&keyword=바지	
+				http://loveloveme.com/product/search.html?banner_action=&keyword=바지
+				http://hotping.co.kr/product/search.html?banner_action=&order_by=favor&keyword=바지
+				http://www.dejou.co.kr/product/search.html?banner_action=&keyword=바지
+				http://imvely.com/product/search.html?view_type=&supplier_code=&category_no=&search_type=product_name&keyword=바지 
+		String path = "http://hotping.co.kr/product/search.html?banner_action=&order_by=favor&keyword=바지";
 		JsoupThread thread = new JsoupThread(path);
 		thread.start();
 	}*/
--- kostyleFINAL/src/main/java/kostyle/search/service/SearchServiceImpl.java
+++ kostyleFINAL/src/main/java/kostyle/search/service/SearchServiceImpl.java
@@ -27,6 +27,7 @@ public class SearchServiceImpl implements SearchService{
 	public List<SearchVO> doSearch(String keyword) {
 		List<String> url = searchDao.getSearchUrl();
 		List<JsoupThread> threads = new ArrayList<>();
+		List<JsoupThread> subThreads = new ArrayList<>();
 		for(int i=0; i<url.size(); i++){
 			System.out.println("url"+i+"번지 주소 확인:"+url.get(i));
 			threads.add(new JsoupThread(url.get(i)+keyword));
@@ -34,18 +35,61 @@ public class SearchServiceImpl implements SearchService{
 		}
 		List<SearchVO> result = new ArrayList<>();
 		List<SearchVO> resultList = null;
-		for(int i=0; i<url.size(); i++){
+		List<String> nextPages = new ArrayList<>();
+	
+		/*for(int i=url.size(); i<0; i--){
 			int count=0;
 			while(count<url.size()){
-				if(threads.get(i).getState()==State.TERMINATED){
-					resultList=threads.get(i).getResult();
-					for(int j=0; j<resultList.size(); j++){
-						result.add(resultList.get(j));
+				if(threads.get(i).getState()==State.TERMINATED){				//쇼핑몰을 검색하는 스레드 중에 종료된 것이 있다면?
+					System.out.println("******"+i+"번째 스레드 종료*********");
+					resultList=threads.get(i).getResult();					 	//리스트에 스레드가 반환하는 리스트를 받음
+					nextPages = threads.get(i).getNextPages();
+					for(int j=0; j<resultList.size(); j++){						//반환값안에 상품객체들이 여러개 있는데...
+						result.add(resultList.get(j));							//각 객체들을 다른 리스트안에 순차적으로 넣음.
 					}
-					threads.get(i).interrupt();
-					count++;
+					threads.get(i).interrupt();									//스레드 종료....
+					count++;													//count변수 1증가
+					break;														//while문으로~~
+				}
+			}
+		}*/
+		/*int count = 0;
+		while(count<url.size()){
+			for(int i=0; i<url.size(); i++){
+				System.out.println("********"+i+"번째 스레드 실행중.");
+				if(threads.get(i).getState()==State.TERMINATED){
+					System.out.println("******"+i+"번째 스레드 종료*********");
+					resultList=threads.get(i).getResult();
+					for(int j=0; j<resultList.size(); j++){						//반환값안에 상품객체들이 여러개 있는데...
+						result.add(resultList.get(j));							//각 객체들을 다른 리스트안에 순차적으로 넣음.
+					}
+				}
+				
+				threads.get(i).interrupt();									//스레드 종료....
+				count++;													//count변수 1증가
+				break;
+			}
+
+		}*/
+		while(threads.size()!=0){
+			for (int i=0 ; i<threads.size(); i++){
+				if(threads.get(i).getState()==State.TERMINATED){
+					System.out.println("******"+i+"번째 스레드 종료*********");
+					resultList=threads.get(i).getResult();					 	//리스트에 스레드가 반환하는 리스트를 받음
+					/*nextPages = threads.get(i).getNextPages();*/
+					for(int j=0; j<resultList.size(); j++){						//반환값안에 상품객체들이 여러개 있는데...
+						result.add(resultList.get(j));							//각 객체들을 다른 리스트안에 순차적으로 넣음.
+					}
+					nextPages = threads.get(i).getNextPages();
+					threads.remove(i);
 					break;
 				}
+			}
+		}
+		if(nextPages != null){
+			for(int i=0; i<nextPages.size(); i++){
+				subThreads.add(new JsoupThread(nextPages.get(i)));
+				subThreads.get(i).run();
 			}
 		}
 		return result;
