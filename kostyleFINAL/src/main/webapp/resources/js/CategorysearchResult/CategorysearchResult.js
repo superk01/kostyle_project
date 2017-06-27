d13c6d6be441f19cbd71b13c00d80dd66cfc2074
SY <SY@LAPTOP-SY>
2017-06-27 13:23:52 +0900

    header_discountURL03

--- kostyleFINAL/src/main/webapp/resources/js/CategorysearchResult/CategorysearchResult.js
+++ kostyleFINAL/src/main/webapp/resources/js/CategorysearchResult/CategorysearchResult.js
@@ -1,3 +1,28 @@
+$(function() {
+	$('.shoplink').on('click',function(e){
+		e.preventDefault();
+		
+		var url = $(this).attr('href');
+		location.href = "#favoriteBodyTop";
+		
+		if($('#CategorysearchIframe').length > 0){
+			$('#CategorysearchIframe').attr("src", url);
+		}else{
+			$('#favoriteBodyTop').remove();
+			$('#favoritebackground').prepend('<div id="iframeRemove">쇼핑몰닫기</div>');
+			$('#favoritebackground').prepend('<iframe id="CategorysearchIframe" width="100%" height="900" src='+'\"'+url+'\"></iframe>');
+			$('#favoritebackground').prepend('<div id="favoriteBodyTop"></div>');
+		}
+		$('#iframeRemove').on('click', function(){
+			$('#CategorysearchIframe').remove();
+			$('#iframeRemove').remove();
+		
+		});
+		
+	});
+});
+
+
 $(function(){	
 	CategoryItemClick();
 	CategoryFilterClick();
@@ -18,6 +43,31 @@ function CategoryItemClick(){
 		});
 	});	
 }
+/*function CategoryItemClick(){
+	$('.CategoryResult_oneItemBox a').click(function(event){
+		var link = $(this).attr('href');
+		location.href = "#CategoryResult_top";
+		event.preventDefault();
+		$('#TotalCategoryResult_Box').parent().prepend(' <div id="IframeRemocon">쇼핑몰 닫기</div> ');
+		$('#TotalCategoryResult_Box').parent().prepend('<iframe id="CategorysearchIframe" width="100%" height="900" src="' +link +'">');
+		
+		
+		$('#IframeRemocon').click(function(){
+			$('#CategorysearchIframe').remove();
+			$('#IframeRemocon').remove();
+		});
+	});	
+}
+*/
+
+
+
+
+
+
+
+
+
 
 function CategoryFilterClick(){
 	
